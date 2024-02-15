0fd006a53cd147072308e26d94a6130450ac8e07
Torsten Schlieder <torsten.schlieder@unite.eu>
2024-02-15 10:54:43 +0100

    Exports, minor adjustments

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
+// Types
+export { Bounds } from './types/Bounds'
 export { Coords } from './types/Coords'
+export { Centroid } from './types/Centroid'
+export { Tile } from './types/Tile'
+export { TileRectangle } from './types/TileRectangle'
+export { TileSquare } from './types/TileSquare'
+// Containers
+export { TileSet } from './containers/TileSet'
+export { TileClusters } from './containers/TileClusters'
+export { ClusterSquare } from './containers/ClusterSquare'
+// Algorithms
+export { coords2tile } from './algorithms/coords2tile'
+export { tile2coords } from './algorithms/tile2coords'
+export { tiles2clusters } from './algorithms/tiles2clusters'
+export { cluster2square } from './algorithms/cluster2square'

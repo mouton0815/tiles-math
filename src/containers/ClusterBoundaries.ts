f76088a68de115fb53deaa20c71e213551da8113
Torsten Schlieder <torsten.schlieder@unite.eu>
2024-02-18 10:20:26 +0100

    More convenient interface of class ClusterBoundaries

--- src/containers/ClusterBoundaries.ts
+++ src/containers/ClusterBoundaries.ts
@@ -5,26 +5,46 @@ import { BoundarySegment } from '../types/BoundarySegment'
 /// Contains all polylines the defined the boundaries of a cluster.
 ///
 export class ClusterBoundaries {
-    boundaries: Array<BoundaryPolyline>
+    array: Array<BoundaryPolyline>
     constructor() {
-        this.boundaries = new Array<BoundaryPolyline>()
+        this.array = new Array<BoundaryPolyline>()
     }
 
+    addUpperEdge(x: number, y: number) {
+        this.prepend(BoundarySegment.fromUpperEdge(x, y))
+    }
+
+    addLeftEdge(x: number, y: number) {
+        this.append(BoundarySegment.fromLeftEdge(x, y))
+    }
+
+    addLowerEdge(x: number, y: number) {
+        this.append(BoundarySegment.fromLowerEdge(x, y))
+    }
+
+    addRightEdge(x: number, y: number) {
+        this.prepend(BoundarySegment.fromRightEdge(x, y))
+    }
+
+    /// Internal method that searches for a matching polyline start to prepend the segment.
+    /// If no such line exits, the method creates a new polyline with the segment.
     prepend(segment: BoundarySegment) {
-        for (const boundary of this.boundaries) {
+        for (const boundary of this.array) {
             if (boundary.tryPrepend(segment)) {
                 return
             }
         }
-        this.boundaries.push(new BoundaryPolyline(segment))
+        this.array.push(new BoundaryPolyline(segment))
     }
 
+    /// Internal method that searches for a matching polyline end to append the segment.
+    /// If no such line exits, the method creates a new polyline with the segment.
     append(segment: BoundarySegment) {
-        for (const boundary of this.boundaries) {
+        for (const boundary of this.array) {
             if (boundary.tryAppend(segment)) {
                 return
             }
         }
-        this.boundaries.push(new BoundaryPolyline(segment))
+        this.array.push(new BoundaryPolyline(segment))
     }
 }

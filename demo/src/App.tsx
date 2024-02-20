7f0c4893c3c4c58223d7935a9e7249686b535076
Torsten Schlieder <torsten.schlieder@unite.eu>
2024-02-20 18:08:50 +0100

    Demo documentation

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -3,24 +3,20 @@ import { MapContainer, TileLayer } from 'react-leaflet'
 import { coords2tile, tile2coords, Coords, Tile, TileSet } from 'tile-math'
 import { Tiles } from './Tiles'
 
-function randomInt(max: number): number {
-    return Math.floor(Math.random() * max);
-}
+// Constants controlling the map view
+const tileZoom = 14 // Zoom level for tile math (14 is the zoom used by VeloViewer and others)
+const squareSize = 10 // Edge length of the map area to be filled with random tiles
+const mapCenter: Coords = [51.482084, 0] // Greenwich
 
-const TILE_ZOOM = 14 // Zoom level for tile math. 14 is the zoom used by VeloViewer and others.
-
-// const center: Coords = [51.335793, 12.371988] // Neues Rathaus
-const center: Coords = [51.482084, 0] // Greenwich
-const centerTile = coords2tile(center, TILE_ZOOM)
-const squareSize = 10 // The area to be filled with random tiles
+// Derived constants
+const centerTile = coords2tile(mapCenter, tileZoom)
 const leftBorder = centerTile.x - squareSize / 2
 const upperBorder = centerTile.y - squareSize / 2
-const mapZoom = TILE_ZOOM - Math.ceil(squareSize / 10) - 1
+const mapZoom = tileZoom - Math.ceil(squareSize / 10) - 1
 const mapBounds = [
     tile2coords(leftBorder, upperBorder, mapZoom),
     tile2coords(leftBorder + squareSize, upperBorder + squareSize, mapZoom)
 ]
-const iterations = squareSize * 30
 const delay = Math.round(1 / (squareSize * squareSize) * 30000)
 
 const initTileSet = new TileSet().add(centerTile)
@@ -29,9 +25,10 @@ export const App = () => {
     const [tileSet, setTileSet] = useState<TileSet>(initTileSet)
 
     useEffect(() => {
+        // Call useEffect repeatedly to add more random tiles
         const timer = (ms: number) => new Promise(res => setTimeout(res, ms));
         (async function() {
-            for (let i = 0; i < iterations; i++) {
+            while (true) {
                 const tile = Tile.of(leftBorder + randomInt(squareSize), upperBorder + randomInt(squareSize))
                 setTileSet(new TileSet(tileSet.add(tile)))
                 await timer(delay)
@@ -41,7 +38,7 @@ export const App = () => {
 
     return (
         <MapContainer
-            center={center}
+            center={mapCenter}
             zoom={mapZoom}
             bounds={mapBounds}
             scrollWheelZoom={true}
@@ -50,7 +47,11 @@ export const App = () => {
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Tiles tiles={tileSet} zoom={TILE_ZOOM} />
+            <Tiles tiles={tileSet} zoom={tileZoom} />
         </MapContainer>
     )
+}
+
+function randomInt(max: number): number {
+    return Math.floor(Math.random() * max);
 }

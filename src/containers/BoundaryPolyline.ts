3e68bfb939f2a98203cc5ccf99955751aaeb4ca0
Torsten Schlieder <torsten.schlieder@unite.eu>
2024-03-04 20:23:13 +0100

    JSDoc of BoundaryPolyline

--- src/containers/BoundaryPolyline.ts
+++ src/containers/BoundaryPolyline.ts
@@ -2,39 +2,69 @@ import { BoundarySegment } from '../types/BoundarySegment'
 import { Coords } from '../types/Coords'
 import { tile2coords } from '../algorithms/tile2coords'
 
+/**
+ * A polyline representing the boundaries of a tile cluster.
+ * Method {@link positions} returns a sequence of positions that can be passed to Leaflet's Polyline map element.
+ */
 export class BoundaryPolyline {
     segments: Array<BoundarySegment>
     zoom: number
 
-    /// Initializes a new cluster boundary with a start segment.
+    /**
+     * Initializes a new {@link BoundaryPolyline} with an initial segment.
+     * @param segment - the initial segment of this polyline.
+     * @param zoom - the zoom level of the tile cluster.
+     */
     constructor(segment: BoundarySegment, zoom: number) {
         this.segments = new Array<BoundarySegment>()
         this.segments.push(segment)
         this.zoom = zoom
     }
 
-    /// Appends the other line to this line.
+    /**
+     * Appends the other polyline to this line.
+     * @param other - the line to append.
+     */
     appendLine(other: BoundaryPolyline) {
         this.segments.push(...other.segments)
     }
 
-    /// Appends the other line to this line.
+    /**
+     * Prepends the other polyline to this line.
+     * @param other - the line to prepend.
+     */
     prependLine(other: BoundaryPolyline) {
         this.segments.unshift(...other.segments)
     }
 
+    /**
+     * Checks whether the given {@link BoundarySegment} can be prepended to this polyline.
+     * That is the case if the end position of the other polyline is equal the start position of this polyline.
+     * @param segment - the {@link BoundarySegment} to prepend.
+     * @returns true iff the passed segment can be prepended, false otherwise.
+     */
     canPrepend(segment: BoundarySegment): boolean {
         const firstSegment = this.segments[0] // Array is never empty
         return segment.x2 === firstSegment.x1 && segment.y2 === firstSegment.y1
     }
 
+    /**
+     * Checks whether the given {@link BoundarySegment} can be appended to this polyline.
+     * That is the case if the start position of the other polyline is equal the end position of this polyline.
+     * @param segment - the {@link BoundarySegment} to append.
+     * @returns true iff the passed segment can be appended, false otherwise.
+     */
     canAppend(segment: BoundarySegment): boolean {
         const lastSegment = this.segments[this.segments.length - 1] // Array is never empty
         return segment.x1 === lastSegment.x2 && segment.y1 === lastSegment.y2
     }
 
-    /// Inserts the new segment before the start of this boundary if the end coordinates of the segment
-    /// are equal to the start coordinates of the boundary. Returns true in this case and false otherwise.
+    /**
+     * Inserts the new segment before the start of this boundary if the end coordinates of the segment
+     * are equal to the start coordinates of the boundary. Returns true in this case and false otherwise.
+     * @param segment - the {@link BoundarySegment} to prepend.
+     * @returns true iff the passed segment was prepended, false otherwise.
+     * */
     tryPrepend(segment: BoundarySegment): boolean {
         const firstSegment = this.segments[0] // Array is never empty
         if (segment.x2 === firstSegment.x1 && segment.y2 === firstSegment.y1) {
@@ -50,8 +80,12 @@ export class BoundaryPolyline {
         return false
     }
 
-    /// Appends the new segment to the end of this boundary if the start coordinates of the new segment
-    /// are equal to the end coordinates of the boundary. Returns true in this case and false otherwise.
+    /**
+     * Appends the new segment to the end of this boundary if the start coordinates of the new segment
+     * are equal to the end coordinates of the boundary. Returns true in this case and false otherwise.
+     * @param segment - the {@link BoundarySegment} to append.
+     * @returns true iff the passed segment was appended, false otherwise.
+     */
     tryAppend(segment: BoundarySegment): boolean {
         const lastSegment = this.segments[this.segments.length - 1] // Array is never empty
         if (segment.x1 === lastSegment.x2 && segment.y1 === lastSegment.y2) {
@@ -67,12 +101,19 @@ export class BoundaryPolyline {
         return false
     }
 
+    /**
+     * Checks if this polyline is circular, i.e. the start and end positions are equal.
+     * @returns true if the polyline is circular, false otherwise.
+     */
     isCircular(): boolean {
         const firstSegment = this.segments[0]
         const lastSegment = this.segments[this.segments.length - 1]
         return firstSegment.x1 === lastSegment.x2 && firstSegment.y1 === lastSegment.y2
     }
 
+    /**
+     * Returns a sequence of coordinates that can be passed to Leaflet's Polyline map element.
+     */
     positions(): Array<Coords> {
         const results = new Array<Coords>()
         let first = true

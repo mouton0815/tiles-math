47ce436ba38716e348fe253b235912783c0584a5
Torsten Schlieder <torsten.schlieder@unite.eu>
2024-03-05 14:09:04 +0100

    Documentation of TileClusters

--- src/containers/TileClusters.ts
+++ src/containers/TileClusters.ts
@@ -1,8 +1,8 @@
 import { TileSet } from './TileSet'
 
-///
-/// Three clusters computed by the {@link tiles2clusters} algorithm.
-///
+/**
+ * Three tile clusters computed by the {@link tiles2clusters} algorithm.
+ */
 export type TileClusters = {
     detachedTiles: TileSet, // Tiles detached from others (with up to three neighbors)
     minorClusters: TileSet, // Tiles with a neighbor on every side (which forms a cluster by definition)

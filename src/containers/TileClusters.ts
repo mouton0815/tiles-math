f60086af1de04f1d30909769463d4341dcac3e53
Torsten Schlieder <torsten.schlieder@unite.eu>
2024-03-20 10:17:39 +0100

    Revert "Compute an extra max cluster with neighborhood tiles"
    This reverts commit 387840877f7c4413ce4e1a7b9b2f4f6e39bbedb3.

--- src/containers/TileClusters.ts
+++ src/containers/TileClusters.ts
@@ -7,5 +7,4 @@ export type TileClusters = {
     readonly detachedTiles: TileSet, // Tiles detached from others (with up to three neighbors)
     readonly minorClusters: TileSet, // Tiles with a neighbor on every side (which forms a cluster by definition)
     readonly maxCluster: TileSet     // Tiles of the maximum cluster
-    readonly maxClusterEx: TileSet   // Tiles of the maximum cluster including their direct neighbors (useful for max-square calculation)
 }

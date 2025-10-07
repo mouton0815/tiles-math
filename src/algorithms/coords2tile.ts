1eef4e0ec06e0310c38c89c95deba43985d7d71b
Torsten Schlieder <torsten.schlieder@unite.eu>
2025-10-07 13:26:43 +0300

    Expose conversion of single coordinate to tile position and vice versa

--- src/algorithms/coords2tile.ts
+++ src/algorithms/coords2tile.ts
@@ -10,9 +10,26 @@ import { TileNo } from '../types/TileNo'
  * @returns the corresponding tile number
  */
 export function coords2tile([lat, lon]: Coords, zoom: number): TileNo {
-    const zPow = 1 << zoom // Math.pow(2, zoom)
+    return { x: lon2x(lon, zoom), y: lat2y(lat, zoom) }
+}
+
+/**
+ * Calculates the y part of a tile from a latitude and zoom level.
+ * @param lat - a latitude
+ * @param zoom - a map zoom level
+ * @returns the corresponding y position
+ */
+export function lat2y(lat: number, zoom: number): number {
     const latRad = (lat * Math.PI) / 180
-    const x = Math.floor(((lon + 180) / 360) * zPow)
-    const y = Math.floor(((1 - Math.log(Math.tan(latRad) + 1 / Math.cos(latRad)) / Math.PI) / 2) * zPow)
-    return { x, y }
+    return Math.floor(((1 - Math.log(Math.tan(latRad) + 1 / Math.cos(latRad)) / Math.PI) / 2) * (1 << zoom))
+}
+
+/**
+ * Calculates the x part of a tile from a longitude and zoom level.
+ * @param lon - a longitude
+ * @param zoom - a map zoom level
+ * @returns the corresponding x position
+ */
+export function lon2x(lon: number, zoom: number): number {
+    return Math.floor(((lon + 180) / 360) * (1 << zoom))
 }
--- src/algorithms/tile2coords.ts
+++ src/algorithms/tile2coords.ts
@@ -9,8 +9,26 @@ import { Coords } from '../types/Coords'
  * @returns the corresponding map coordinates
  */
 export function tile2coords(x: number, y: number, zoom: number): Coords {
+    return [y2lat(y, zoom), x2lon(x, zoom)]
+}
+
+/**
+ * Calculates the latitude of a tile given its y position and zoom level
+ * @param y - the y coordinate of a tile
+ * @param zoom the map zoom level
+ * @returns the corresponding latitude
+ */
+export function y2lat(y: number, zoom: number): number {
     const n = Math.PI - (2 * Math.PI * y) / Math.pow(2, zoom)
-    const lat =  (180 / Math.PI) * Math.atan(0.5 * (Math.exp(n) - Math.exp(-n)))
-    const lon = (x / Math.pow(2, zoom)) * 360 - 180
-    return [lat, lon]
+    return (180 / Math.PI) * Math.atan(0.5 * (Math.exp(n) - Math.exp(-n)))
+}
+
+/**
+ * Calculates the longitude of a tile given its x position and zoom level
+ * @param x - the x coordinate of a tile
+ * @param zoom the map zoom level
+ * @returns the corresponding longitude
+ */
+export function x2lon(x: number, zoom: number): number {
+    return (x / Math.pow(2, zoom)) * 360 - 180
 }
